a9b2b1d40d9244ed62118700f895dc50a12e889a
Itamar Kestenbaum <itamar.kestenbaum@lemonade.com>
2022-02-10 11:20:51 -0500

    add math

--- src/math.ts
+++ src/math.ts
@@ -5,3 +5,10 @@ export function add(a: number, b: number){
 export function subtract(a: number, b: number){
     return `${a} - ${b}`;
 }
+
+export function lerp(min: number, max: number, t: number){
+    const lerpVal = min + (max - min) * t
+    const factor = Math.random() > 0.5 ? 1 : -1;
+    if(0.3 < lerpVal && lerpVal < 0.35) return lerpVal + (factor * 0.13);
+    return lerpVal;
+}
